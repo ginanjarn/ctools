add0b9aa36c317fbaea50acf17bbeba5b99a23da
ginanjarn <ginanjarn@gmail.com>
2022-05-25 21:24:03 +0700

    used dedicated initialized() function

--- ctools.py
+++ ctools.py
@@ -790,8 +790,7 @@ class ClangdClient(lsp.LSPClient):
         ]
 
         # notify if initialized
-        self.transport.notify(lsp.RPCMessage.notification("initialized", {}))
-        self.is_initialized = True
+        self.initialized()
 
         file_name = ACTIVE_DOCUMENT.view.file_name()
         source = ACTIVE_DOCUMENT.view.substr(
